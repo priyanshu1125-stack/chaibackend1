7618892d45b6cf052fe1937827a372b6ff708b8f
priyanshu1125-stack <priyanshu1125kumar@gmail.com>
2025-05-18 11:16:00 +0530

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+// console.log("chai or code")
 
 require('dotenv').config()
 const express = require('express')
-
-
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/twitter', (req, res) => {
-  res.send('hiteshdotcom')
-})
-app.get('/login',(req,res) => {
-    res.send('<h1>plese login at chai and code</h1>')
+
+app.get('/twitter',(req,res)=>{
+    res.send('priyanshu.com')
 })
 
-app.get('/youtube',(req,res) => {
+app.get('/login',(req,res)=>{
+    res.send('<h1>plese login at chai or code</h1>')
+})
+
+app.get('/youtube',(req,res)=>{
     res.send('<h2>chai or code</h2>')
 })
-app.listen(process.env.PORT, () => {
+
+app.listen(process.env.port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
